4f808275d8b698b1a47a0b2a9ec89113bbefba4d
Neuralearn <team@neuralearn.ai>
2024-02-11 18:32:23 +0100

    updatae init file without labels.csv

--- __init__.py
+++ __init__.py
@@ -1312,6 +1312,15 @@ def list_classes(mask_path):
   return list_unique
 
 def transform_sample(sample, select_class, prompt, num_images_per_prompt, guidance_scale, num_inference_steps):
+    id2label = {0: 'nan',1: 'accessories',2: 'bag',3: 'belt',4: 'blazer',5: 'blouse',6: 'bodysuit',7: 'boots',8: 'bra',
+            9: 'bracelet',10: 'cape',11: 'cardigan',12: 'clogs',13: 'coat',14: 'dress',15: 'earrings',16: 'flats',
+            17: 'glasses',18: 'gloves',19: 'hair',20: 'hat',21: 'heels',22: 'hoodie',23: 'intimate',24: 'jacket',25: 'jeans',
+            26: 'jumper',27: 'leggings',28: 'loafers',29: 'necklace',30: 'panties',31: 'pants',32: 'pumps',33: 'purse',
+            34: 'ring',35: 'romper',36: 'sandals',37: 'scarf',38: 'shirt',39: 'shoes',40: 'shorts',41: 'skin',42: 'skirt',
+            43: 'sneakers',44: 'socks',45: 'stockings',46: 'suit',47: 'sunglasses',48: 'sweater',49: 'sweatshirt',50: 'swimwear',
+            51: 't-shirt',52: 'tie',53: 'tights',54: 'top',55: 'vest',56: 'wallet',57: 'watch',58: 'wedges'}
+    label2id = {label: id for id, label in id2label.items()}
+
     hash = create_hash()
     filename = sample.filepath.split("/")[-1][:-4]+"_"+str(hash)+".png"
     pipe = create_pipeline(model_id)
@@ -1372,10 +1381,15 @@ class SDAugment(foo.Operator):
         )
 
         target_view = ctx.view.select(ctx.selected)
-        labels_filepath = "/assets/labels.csv"
-
-        id2label = labels_from_id(labels_filepath)
-        
+        labels_filepath = "labels.csv"
+        id2label = {0: 'nan',1: 'accessories',2: 'bag',3: 'belt',4: 'blazer',5: 'blouse',6: 'bodysuit',7: 'boots',8: 'bra',
+            9: 'bracelet',10: 'cape',11: 'cardigan',12: 'clogs',13: 'coat',14: 'dress',15: 'earrings',16: 'flats',
+            17: 'glasses',18: 'gloves',19: 'hair',20: 'hat',21: 'heels',22: 'hoodie',23: 'intimate',24: 'jacket',25: 'jeans',
+            26: 'jumper',27: 'leggings',28: 'loafers',29: 'necklace',30: 'panties',31: 'pants',32: 'pumps',33: 'purse',
+            34: 'ring',35: 'romper',36: 'sandals',37: 'scarf',38: 'shirt',39: 'shoes',40: 'shorts',41: 'skin',42: 'skirt',
+            43: 'sneakers',44: 'socks',45: 'stockings',46: 'suit',47: 'sunglasses',48: 'sweater',49: 'sweatshirt',50: 'swimwear',
+            51: 't-shirt',52: 'tie',53: 'tights',54: 'top',55: 'vest',56: 'wallet',57: 'watch',58: 'wedges'}
+        #id2label = labels_from_id(labels_filepath)
         
         for sample in target_view:
             mask = cv2.imread(sample.ground_truth.mask_path,cv2.IMREAD_GRAYSCALE)
