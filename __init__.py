f473ab4af17d5058f44b6c5475f3f7c677f05b48
Neuralearn <team@neuralearn.ai>
2024-02-13 10:03:44 +0100

    add utilspy

--- __init__.py
+++ __init__.py
@@ -32,68 +32,28 @@ id2label = {0: 'nan',1: 'accessories',2: 'bag',3: 'belt',4: 'blazer',5: 'blouse'
     43: 'sneakers',44: 'socks',45: 'stockings',46: 'suit',47: 'sunglasses',48: 'sweater',49: 'sweatshirt',50: 'swimwear',
     51: 't-shirt',52: 'tie',53: 'tights',54: 'top',55: 'vest',56: 'wallet',57: 'watch',58: 'wedges'}
 
-def create_pipeline(model_id):
-  scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-  pipe = StableDiffusionInpaintPipeline.from_pretrained(model_id,
-                                                        scheduler=scheduler,
-                                                        revision="fp16",
-                                                        torch_dtype=torch.float16)
-
-  pipe = pipe.to("cuda")
-  pipe.enable_xformers_memory_efficient_attention()
-  return pipe
-
-def generate_inputs(im_path,mask_path, mask_id):
-  print("the mask id is ===", mask_id)
-  #select_class = id2label[mask_id]
-
-  source_image = cv2.imread(im_path)
-  print("sourceimg", source_image.shape)
-  sd_mask = cv2.imread(mask_path,cv2.IMREAD_GRAYSCALE)
-
-  out = (sd_mask+(-mask_id*np.ones_like(sd_mask)))
-  mask=-(np.clip(1e10*np.multiply(out,out),a_min=0,a_max=255)-255)
-  pil_image = Image.fromarray(source_image).resize((512,512))
-  pil_mask = Image.fromarray(mask).resize((512,512))
-  return pil_image, pil_mask
-
-def augpaint(pipe, prompt, pil_image, pil_mask, num_images_per_prompt, guidance_scale, num_inference_steps,random_seed):
-
-  # Generate a random seed
-  generator = torch.Generator(device="cuda").manual_seed(random_seed)
-
-  # Run stable diffusion pipeline in inpainting mode and store the generated images in a list
-  encoded_images = []
-  for i in range(num_images_per_prompt):
-      image = pipe(prompt=prompt, guidance_scale=guidance_scale,
-                    num_inference_steps=num_inference_steps, generator=generator,
-                    image=pil_image, mask_image=pil_mask).images[0]
-      encoded_images.append(image.resize((550,825)))
-  return encoded_images
-
-def create_hash():
-  randint = random.randint(0, 100000000)
-  hash = hashlib.sha256(str(randint).encode("utf-8")).hexdigest()[:10]
-  return hash
-
-def list_classes(mask_path):
-
-  mask = cv2.imread(mask_path,cv2.IMREAD_GRAYSCALE)
-  list_unique = np.unique(mask)
-  return list_unique
-
+with add_sys_path(os.path.dirname(os.path.abspath(__file__))):
+    # pylint: disable=no-name-in-module,import-error
+    
+    from utils import (
+        _create_pipeline,
+        _create_hash,
+        _augpaint,
+        _generate_inputs,
+    )
+    
 def transform_sample(sample, select_class, prompt, num_images_per_prompt, guidance_scale, num_inference_steps, random_seed):
     
     label2id = {label: id for id, label in id2label.items()}
 
-    hash = create_hash()
+    hash = _create_hash()
     filename = sample.filepath.split("/")[-1][:-4]+"_"+str(hash)+".png"
-    pipe = create_pipeline(model_id)
+    pipe = _create_pipeline(model_id)
     im,mask = generate_inputs(
         sample.filepath, sample.ground_truth.mask_path,
         label2id[select_class])
 
-    output_images = augpaint(pipe, prompt, im, mask,num_images_per_prompt, guidance_scale, num_inference_steps, random_seed)
+    output_images = _augpaint(pipe, prompt, im, mask,num_images_per_prompt, guidance_scale, num_inference_steps, random_seed)
     new_samples = []
     for i,out in enumerate(output_images):
         cv2.imwrite(sample.filepath[:-4]+"_"+str(hash)+"_"+str(i)+".png",
@@ -110,17 +70,6 @@ def transform_sample(sample, select_class, prompt, num_images_per_prompt, guidan
         ))
 
     return new_samples
-
-def labels_from_id(labels_filepath):
-    df = pd.read_csv(labels_filepath)
-    id2label = {}
-    for i,j in df.iterrows():
-        if i==0:
-            id2label[i] = 'nan'
-        else:
-            id2label[i] = j['label_list']
-    return id2label
-
 
 class SDAugment(foo.Operator):
     @property
@@ -150,8 +99,6 @@ class SDAugment(foo.Operator):
         )
 
         target_view = ctx.view.select(ctx.selected)
-        #labels_filepath = "labels.csv"
-        #id2label = labels_from_id(labels_filepath)
         
         for sample in target_view:
             mask = cv2.imread(sample.ground_truth.mask_path,cv2.IMREAD_GRAYSCALE)
